07bc62a6cd972574ff9ccb5a9d318b9c772165e7
Sergi Ortega M <sortega@digitalorigin.com>
2017-11-29 15:24:18 +0100

    feat: Multisamples [WIP]

--- custom/init.js
+++ custom/init.js
@@ -1,24 +1,31 @@
 const fs = require('fs');
+const path = require('path');
 const rimraf = require('rimraf');
 const _sample = require('lodash/sample');
-const baseDir = 'base';
+const sampleBaseDir = './baseSounds';
+const sampleDirs = fs.readdirSync(sampleBaseDir);
 
-const first = ['0', '1', '2', '3', '4', '5', '6', '7'];
-const second = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-const third = ['0', '1'];
 
-const keyStrokesUP = fs.readdirSync('./baseUP');
-const keyStrokesDOWN = fs.readdirSync('./baseDOWN');
+sampleDirs.forEach(sample => {
+  const first = ['0', '1', '2', '3', '4', '5', '6', '7'];
+  const second = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+  const third = ['0', '1'];
 
-first.forEach(f => {
-  second.forEach(s => {
-    third.forEach(t => {
-      if (t === '0') {
-        fs.createReadStream(`./baseDOWN/${_sample(keyStrokesDOWN)}`).pipe(fs.createWriteStream(`output/${f}${s}-${t}.wav`));
-      }
-      if (t === '1') {
-        fs.createReadStream(`./baseUP/${_sample(keyStrokesUP)}`).pipe(fs.createWriteStream(`output/${f}${s}-${t}.wav`));
-      }
-    })
-  })
-})
+  const baseUPDir = `./tmp/${sample}/baseUP`;
+  const baseDOWNDir = `./tmp/${sample}/baseDOWN`;
+  const keyStrokesUP = fs.readdirSync(baseUPDir);
+  const keyStrokesDOWN = fs.readdirSync(baseDOWNDir);
+
+  first.forEach(f => {
+    second.forEach(s => {
+      third.forEach(t => {
+        if (t === '0') {
+          fs.createReadStream(`${baseDOWNDir}/${_sample(keyStrokesDOWN)}`).pipe(fs.createWriteStream(`output/${sample}/${f}${s}-${t}.wav`));
+        }
+        if (t === '1') {
+          fs.createReadStream(`${baseUPDir}/${_sample(keyStrokesUP)}`).pipe(fs.createWriteStream(`output/${sample}${f}${s}-${t}.wav`));
+        }
+      });
+    });
+  });
+});
